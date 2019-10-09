4d83647e459db79cd28680af320835bc376d6574
na <narrth@gmail.com>
2019-10-09 19:20:30 -0400

    first commit

---
+++ Gradebook/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gradebook
+{
+    class Book
+    {
+        //constructor methods are completed in methods with names matching the class
+        //constructor methods are used to allocate memory for variables, but do not create the variables yet
+        public Book(string name)    //the brackets "name" allow us to pass a variable in the form string to the method
+        {
+
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        //"new" is not needed for non-object type variables
+        //"this." allows us to reffer directly to a member of the class instead of the string"name" with the same name
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade); 
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+            //var is a blank type variable , the console will recognize what type I need
+
+
+            foreach (var number in grades) //can use array(numbers) or list(grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowestGrade = Math.Min(number, lowestGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($" The lowest grade is {lowestGrade:N1}");
+            Console.WriteLine($" The highest grade is {highGrade:N1}");
+            Console.WriteLine($" The average is {result:N1}");
+            //"$" is used for odd string outputs
+            //":N#" allows us to format numbers with the desired truncation
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+
+}
---
+++ Gradebook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Program
+    {
+        //static methods can not be instance and must be directly called
+        //there is only one in existance unlike objects that have mukltiple existances
+        static void Main(string[] args)
+        {
+            var book = new Book($"Narr's Gradebook");
+            book.AddGrade(87.1);
+            book.AddGrade(60.7);
+            book.AddGrade(90.4);
+            book.ShowStatistics();
+
+            /*var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.2);
+            //press "crtl" + "." beside and error to see suggestions then selct one
+            //list is a unique array with many built in functions, it requires an 
+            //addition parameter indicating the type of list in "<" and ">" brackets*/
+
+            
+
+        }
+    }
+}
